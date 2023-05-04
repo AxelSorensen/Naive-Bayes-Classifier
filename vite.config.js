d306aef839f28e6539cc9f09e58180a416c72583
Axel Sorensen <Axel123@live.dk>
2023-05-04 11:19:40 +0200

    added PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Naive-Bayes-Classifier/',
-  plugins: [vue()],
+  plugins: [vue(),VitePWA({registerType: 'autoUpdate', manifest: {
+    "name": "Explorable: Naive Bayes Classifier",
+    "icons": [
+      {
+        "src": "icons/bayes_icon.png",
+        "sizes": "512x512",
+        "type": "image/png"
+      }
+    ]
+  }})],
   optimizeDeps: {
     exclude: ['vue-mathjax']
   }
